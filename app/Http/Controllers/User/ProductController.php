bdbcf3332cfd5df6b3f7a1dd472bfbfa5daf05e0
fancycoder328 <fantasticcoder999@gmail.com>
2024-01-05 14:13:49 +0200

    change relations

--- app/Http/Controllers/User/ProductController.php
+++ app/Http/Controllers/User/ProductController.php
@@ -6,11 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
-    public function index() {
-        $products = Product::with('category','tags')->inRandomOrder()->paginate(50);
+    public function index()
+    {
+        $products = Product::with('media', 'tags:id,name', 'category:id,name', 'discounts')->inRandomOrder()->paginate(50);
         return ProductResource::collection($products);
     }
 }
